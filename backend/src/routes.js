c261b7347b637654437967c2aaddc87330a976e3
anderfilth <anderfilth@hotmail.com>
2020-01-18 17:35:41 -0300

    add eslint config

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,6 @@ const { Router } = require('express');
 
 const routes = Router();
 
-routes.get('/', (Request, response) => {
-  return response.json({ message: 'hello!' });
-});
+routes.get('/', (Request, response) => response.json({ message: 'hello!' }));
 
 module.exports = routes;
