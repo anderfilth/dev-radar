aaf73a097c7b315f505d3858fb69474b7e8a2c5e
anderfilth <anderfilth@hotmail.com>
2020-01-18 17:00:46 -0300

    add routes

---
+++ backend/src/routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express');
+
+const routes = Router();
+
+routes.get('/', (Request, response) => {
+    return response.json({ message: 'hello!' });
+});
+
+module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 
+const routes = require('./routes');
+
 const app = express();
 
-app.get('/', (Request, response) => {
-    return response.json({ message: 'hello!' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
