f01d37e3f8ae3a77a6f633a4272489e1f54f99a4
anderfilth <anderfilth@hotmail.com>
2020-01-19 17:06:47 -0300

    add mobile

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
 
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 require('./database');
 
 const app = express();
+const server = http.Server(app);
+setupWebsocket(server);
 
 app.use(cors());
 app.use(express.json());
