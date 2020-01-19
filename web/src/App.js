09c3d2de408037142f77d53dfd49f0690d65a5da
anderfilth <anderfilth@hotmail.com>
2020-01-18 22:28:18 -0300

    add list of devs

--- web/src/App.js
+++ web/src/App.js
@@ -1,28 +1,30 @@
 import React, {useState, useEffect} from 'react';
 
+import api from './services/api';
+
 import './global.css';
 import './App.css';
 import './Sidebar.css'
 import './Main.css'
 
 import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
   const [devs, setDevs] = useState([]);
 
   useEffect(() => {
     async function loadDevs() {
-      const response = {};
+      const response = await api.get('/devs');
 
       setDevs(response.data);
     }
 
     loadDevs();
-  }, [devs]);
+  }, []);
 
   async function handleAddDev(data) {
-    const response = {};
-    // const response = await api.post('/devs', data);
+    const response = await api.post('/devs', data);
     const hasDev = devs.filter(dev => dev.github_name === data.github_name);
     
     if(!hasDev) {
@@ -35,7 +37,13 @@ function App() {
         <strong>Cadastrar</strong>
         <DevForm onSubmit={handleAddDev} />
       </aside>
-      <main></main>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}          
+        </ul>
+      </main>
     </div>
   );
 }
